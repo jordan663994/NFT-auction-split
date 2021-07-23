e9b5e3e60e5f1f01045e6602d0d7647e5843c8dc
jordan69420 <58832117+jordan69420@users.noreply.github.com>
2021-07-23 14:00:55 -0400

    Update NFTshare.sol

--- NFTshare.sol
+++ NFTshare.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.8.0;
 
 contract nftShare {
-   address[5] group;
+   address[4] group;
    struct nft {
       group shareholders;
    }
    function sell_nft(string memory add_args) {
       //add code to transfer the nft and profits + 10% here
-      
+      for(uint i=0; i <=4; i++) {
+         address fund_reciever = nft.shareholders[i]
+         //add code to transfer 20% of the fee to the address fund_reciever
+      }
 }
