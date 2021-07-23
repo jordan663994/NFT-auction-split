cfdd89582ef8dda43823cc23f6a4179f3f71da98
jordan69420 <58832117+jordan69420@users.noreply.github.com>
2021-07-23 13:56:30 -0400

    Create NFTshare.sol

---
+++ NFTshare.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+contract nftShare {
+   address[5] group;
+   struct nft {
+      group shareholders;
+   }
+   function sell_nft(string memory add_args) {
+      //add code to transfer the nft and profits + 10% here
+      
+}
